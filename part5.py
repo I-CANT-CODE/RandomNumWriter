4f2c297da4423e2e8504b5f7e98157d9f26f3104
robbie sadre <rssadre@ucdavis.edu>
2018-08-14 17:15:44 -0700

    program now terminates
    program can now be terminated with ctrl C

--- part5.py
+++ part5.py
@@ -10,69 +10,66 @@ import random
 class NumberSim:
     history = queue.Queue(100)
     
-    
+    FLAG = False
     def __init__(self):
         self.count = 0
         self.recent_num = 0
         self.FILE = open('history_file.txt','w')
         self.FILE.close()
         
+        
     def RandNumGen(self):
-        while True:
+        while self.FLAG == False:
             num = numpy.random.choice(numpy.arange(1,6),p = [.5,.25,.15,.05,.05])
             self.recent_num = num
+            time.sleep(.5)
             ctime = datetime.datetime.now().time()
-            
+            #print("{} {}\n".format(num,ctime))
         #if self.history.full():#if q is full then remove oldest item
             #self.history.get()
             self.history.put([num,ctime])#add number
-            time.sleep(.1)
+            
         #print(num,time)
 
 
-    def PrintHistory(self): #for testing purposes only
-        while not self.history.empty():
-            print(self.history.get())
+    
 
     def Writer(self):
-        while True:
+        while self.FLAG == False:
             self.FILE = open('history_file.txt','a')
             inst = self.history.get()
             self.FILE.write(str(inst[0]) + ' , '  + str(inst[1]) +'\n' )
-            time.sleep(0)
-            print('len of q:{}'.format(self.history.qsize()))
+            
+            #print('len of q:{}'.format(self.history.qsize()))
 
-    def PrintQlen(self):
-        print('current length of q',self.history.qsize())
-        
-    
-        
+           
         
 
 # test the function:
 
 numbersim = NumberSim()
+try:
+    for i in range(5):
+        NumGenT = threading.Thread(target = numbersim.RandNumGen, args = (), name = 't{}'.format(i))
+        NumGenT.start()
+        print('{} has launched'.format(NumGenT.name))
 
-for i in range(5):
-    NumGenT = threading.Thread(target = numbersim.RandNumGen, args = (), name = 't{}'.format(i))
-    NumGenT.start()
-    print('{} has launched'.format(NumGenT.name))
 
-
-WriteT = threading.Thread(target = numbersim.Writer, args = (), name = 't_w')
-WriteT.start()
-print('{} has launched'.format(WriteT.name))
-
+    WriteT = threading.Thread(target = numbersim.Writer, args = (), name = 't_w')
+    WriteT.start()
+    print('{} has launched'.format(WriteT.name))
+    while True:
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    numbersim.FLAG = True
+    
 
 
 
 
     
     
-
-
-numbersim.PrintHistory() #ensure that only last 100 items were saved into q 
-
 
 
     
